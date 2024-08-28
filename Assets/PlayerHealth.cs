15f5527e79329e93b82468e8ab38fe36994330ae
Tanpatel <101955781+Tanpatel@users.noreply.github.com>
2024-08-28 00:43:14 -0500

    armor and Energy Health-Bar

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
+
     public int currentHealth;
+    public int currentEnergy;
+    public int currentArmor;
 
     public HealthBar healthBar;
+    public HealthBar energyShield;
+    public HealthBar armorDial;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        currentEnergy = maxHealth;
+        currentArmor = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        energyShield.SetMaxHealth(maxHealth);
+        armorDial.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -20,13 +29,29 @@ public class PlayerHealth : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            TakeDamage(20);
+            TakehealthDamage(20);
+            TakeEnergyDamage(10);
+            TakeArmorDamage(15);
         }
+        
     }
 
-    void TakeDamage(int damage)
+    void TakehealthDamage(int healthdamage)
     {
-        currentHealth -= damage;
+        currentHealth -= healthdamage;
         healthBar.SetHealth(currentHealth);
+
+    }
+
+    void TakeEnergyDamage(int energydamage)
+    {
+        currentEnergy -= energydamage;
+        energyShield.SetHealth(currentEnergy);
+    }
+
+    void TakeArmorDamage(int armorDamage)
+    {
+        currentArmor -= armorDamage;
+        armorDial.SetHealth(currentArmor);
     }
 }
