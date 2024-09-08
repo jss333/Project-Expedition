16f9e80fbb2a46ccba052495bb8298de6081b82d
jss333 <jss333@gmail.com>
2024-09-07 22:12:21 -0400

    Remove code for tracking projectile which is currently unused in the game

--- Assets/Scripts/Projectile_Sys.cs
+++ Assets/Scripts/Projectile_Sys.cs
@@ -1,14 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile_Sys : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private GameObject trackedEntity = null;
 
     [Header("Movement Properties")]
-    public ProjectileType type = ProjectileType.Precision;
     public float speed = 20f;
     public int damageAmt = 2;
 
@@ -16,46 +12,11 @@ public class Projectile_Sys : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        trackedEntity = FindNearestTarget();
     }
     
     void FixedUpdate()
     {
-        if(type == ProjectileType.Tracking && trackedEntity != null){
-            GuideTrackingProjectile();
-        }
-        else{
-            GuidePrecisionProjectile();
-        }
-    }
-
-    private GameObject FindNearestTarget()
-    {
-        //Need to adjust the hierarchy before generalizing this. Consult with Rakshaan.
-        GameObject Target = GameObject.Find("Boss");
-        if (Target == null)
-        {
-            Debug.Log("Couldn't find target...");
-        }
-        return Target;
-    }
-
-    void GuideTrackingProjectile()
-    {
-        //Aligns Tracker velocity vec with vec pointing at Target from Tracker by applying Torque
-        Vector2 distanceToTargetFromProjectile = trackedEntity.transform.position - this.transform.position;
-        float angleToTarget = Vector2.SignedAngle(distanceToTargetFromProjectile, rb.velocity);
-
-        if (angleToTarget != 0)
-        {
-            rb.AddTorque(-angleToTarget);
-        }
-        else if (angleToTarget < 0)
-        {
-            rb.AddTorque(-angleToTarget);
-        }
-
-        rb.velocity = this.transform.right * speed;
+        GuidePrecisionProjectile();
     }
 
     void GuidePrecisionProjectile()
@@ -73,11 +34,4 @@ public class Projectile_Sys : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-}
-
-
-public enum ProjectileType
-{
-    Precision,
-    Tracking
 }
