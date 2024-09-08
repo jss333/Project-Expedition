a2de6a814fcd1be2a939e6713dea6c70a908755c
jss333 <jss333@gmail.com>
2024-09-08 19:47:37 -0400

    Refactor Boss script

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,23 +4,24 @@ using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
-    public GameObject orbPrefab;  // Assign the orb prefab in the inspector
-    public Transform player;      // Assign the player's transform in the inspector
-    public Sprite beatenUpSprite; // Assign the beaten up sprite in the inspector
-    public float orbSpeed = 5f;   // Speed of the orb
-    public float shootInterval = 2f; // Interval between shots
-    private float shootTimer = 0f;
-    private SpriteRenderer spriteRenderer;
+    [Header("References")]
+    public GameObject orbPrefab;
+    public Transform playerPosition;
+    public HealthBar healthBar;
     private Animator bossAnimator;
 
+    [Header("Parameters")]
     public int maxHealth = 100;
     public int currentHealth;
 
-    public HealthBar healthBar;  // Link to the health bar UI element
+    [Header("Parameters - Orb")]
+    public float orbSpeed = 5f;
+    public float orbShotIntervalSec = 2f;
+    private float shotTimer = 0f;
 
+    
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         bossAnimator = GetComponent<Animator>();
 
         // Initialize boss health
@@ -31,18 +32,18 @@ public class BossController : MonoBehaviour
 
     void Update()
     {
-        shootTimer += Time.deltaTime;
+        shotTimer += Time.deltaTime;
 
-        if (shootTimer >= shootInterval)
+        if (shotTimer >= orbShotIntervalSec)
         {
             ShootOrb();
-            shootTimer = 0f;
+            shotTimer = 0f;
         }
     }
 
-    void ShootOrb()
+    private void ShootOrb()
     {
-        Vector2 direction = (Vector2)Vector3.Normalize(player.position - this.transform.position);
+        Vector2 direction = (Vector2)Vector3.Normalize(playerPosition.position - this.transform.position);
         GameObject orb = Instantiate(orbPrefab, transform.position, Quaternion.identity);
         orb.GetComponent<Rigidbody2D>().velocity = direction * orbSpeed;
     }
@@ -59,7 +60,7 @@ public class BossController : MonoBehaviour
         }
     }
 
-    void Die()
+    private void Die()
     {
         Destroy(this.gameObject);
     }
