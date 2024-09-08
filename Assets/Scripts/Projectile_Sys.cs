2429b9243c609bc41fab86d0f6b6831556fabcc5
jss333 <jss333@gmail.com>
2024-09-07 22:07:21 -0400

    Refactor Projectile code to improve readability and remove dead code and comments

--- Assets/Scripts/Projectile_Sys.cs
+++ Assets/Scripts/Projectile_Sys.cs
@@ -4,36 +4,65 @@ using UnityEngine;
 
 public class Projectile_Sys : MonoBehaviour
 {
-    public Team[] TargetTeams = new Team[5];
-    private GameObject TrackEntity = null;
-    
-    [Header("Movement Properties")]
-    public float pos_vel = 2F;
-    public float pos_acc = 30F;
-    public float rot_vel = 5F;
-    public float rot_acc = 180F;
-    public ProjectileType type = ProjectileType.Precision;
-    public int damageAmt = 2;
-    public float speed = 10f;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private GameObject trackedEntity = null;
 
-    // Update is called once per frame
-    void Start(){
+    [Header("Movement Properties")]
+    public ProjectileType type = ProjectileType.Precision;
+    public float speed = 20f;
+    public int damageAmt = 2;
+
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-        TrackEntity = FindNearestTarget();
-        rb.velocity = transform.up * speed;
+        trackedEntity = FindNearestTarget();
     }
     
     void FixedUpdate()
     {
-        if(type == ProjectileType.Tracking && TrackEntity != null){
+        if(type == ProjectileType.Tracking && trackedEntity != null){
             GuideTrackingProjectile();
         }
         else{
             GuidePrecisionProjectile();
         }
     }
+
+    private GameObject FindNearestTarget()
+    {
+        //Need to adjust the hierarchy before generalizing this. Consult with Rakshaan.
+        GameObject Target = GameObject.Find("Boss");
+        if (Target == null)
+        {
+            Debug.Log("Couldn't find target...");
+        }
+        return Target;
+    }
+
+    void GuideTrackingProjectile()
+    {
+        //Aligns Tracker velocity vec with vec pointing at Target from Tracker by applying Torque
+        Vector2 distanceToTargetFromProjectile = trackedEntity.transform.position - this.transform.position;
+        float angleToTarget = Vector2.SignedAngle(distanceToTargetFromProjectile, rb.velocity);
+
+        if (angleToTarget != 0)
+        {
+            rb.AddTorque(-angleToTarget);
+        }
+        else if (angleToTarget < 0)
+        {
+            rb.AddTorque(-angleToTarget);
+        }
+
+        rb.velocity = this.transform.right * speed;
+    }
+
+    void GuidePrecisionProjectile()
+    {
+        rb.velocity = this.transform.right * speed;
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -44,43 +73,11 @@ public class Projectile_Sys : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    
-    void OnBecameInvisible(){
-        //Debug.Log("Self-destruct...");
-        Destroy(this.gameObject);
-    }
-
-    void GuidePrecisionProjectile(){
-        //rb.AddForce(transform.right * pos_acc);
-        rb.velocity = this.transform.right * pos_vel;
-    }
-
-    void GuideTrackingProjectile(){
-        //Aligns Tracker velocity vec with vec pointing at Target from Tracker by applying Torque
-        Vector2 delta = TrackEntity.transform.position - this.transform.position;
-        float AngularDist = Vector2.SignedAngle(delta, rb.velocity);
-        if(AngularDist > 0){
-            rb.AddTorque(-AngularDist);
-        }
-        else if(AngularDist < 0){
-            rb.AddTorque(-AngularDist);
-        }
-        
-        //rb.AddForce(this.transform.right * pos_acc);
-        rb.velocity = this.transform.right * pos_vel;
-    }
-
-    private GameObject FindNearestTarget(){
-        //Need to adjust the hierarchy before generalizing this. Consult with Rakshaan.
-        GameObject Target = GameObject.Find("Boss");
-        if(Target == null){
-            Debug.Log("Couldn't find target...");
-        }
-        return Target;
-    }
 }
 
-public enum ProjectileType{
+
+public enum ProjectileType
+{
     Precision,
     Tracking
 }
