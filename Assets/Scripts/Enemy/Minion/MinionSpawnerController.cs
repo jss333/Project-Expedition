278a2de8bd76cfc1118daf60d82c929e59b5a0b5
Jacob Hooten <jghooten2002@gmail.com>
2024-09-24 16:24:03 -0500

    Anchors are now invisible on play.

--- Assets/Scripts/Enemy/Minion/MinionSpawnerController.cs
+++ Assets/Scripts/Enemy/Minion/MinionSpawnerController.cs
@@ -7,7 +7,7 @@ public class MinionSpawnerController : MonoBehaviour
     public GameObject entityToSpawn;
     private int activeEntityInstances = 0;
     public int entityCount = 2;
-    private float timer = 0;
+    //private float timer = 0;
     [SerializeField] private List<GameObject> freeAnchors; //without minion
     [SerializeField] private List<GameObject> activeAnchors; //with minion
 
