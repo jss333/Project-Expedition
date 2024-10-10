e63d15a5f282b89a1d8b094167e852a0bba4672d
Doryan D Lewis <xgcminx@gmail.com>
2024-10-10 13:25:29 -0500

    Krism Testing Levels Update
    Fixed issues with previous levels (broken walls and player getting stuck)
    Updated tutorial level
    -Added enemy for door opening
    -Added platforms for player to practice climbing
    
    Known Issue:
    -Shield doesn't activate during the tutorial level

---
+++ Assets/Scenes/Level/KrismTestingLevels/TutorialLevelNotes.cs
@@ -0,0 +1,9 @@
+//Notes for the Tutorial level
+
+//Stage 1: The player will learn how to jump to traverse over obsticals
+
+//Stage 2: The player will need to destroy the enemy training dummy to unlock the door to progress further
+
+//Stage 3: The player will need to use the platforms to climb up to the next level to progress forward
+
+//Stage 4: Players will need to use their long jump to cross the opening, while dealing with an enemy
