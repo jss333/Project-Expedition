dcadce57f19f7545a5428858ab52a4d9f4c43e80
jss333 <jss333@gmail.com>
2024-10-20 11:09:27 -0400

    Add background and BGM to Credits screen
    Also button sizes, highlight and pointer the Main screen

--- Assets/Scripts/UI/Credits/ReadTextFile.cs
+++ Assets/Scripts/UI/Credits/ReadTextFile.cs
@@ -7,20 +7,19 @@ using UnityEngine.SceneManagement;
 
 public class ReadTextFile : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI m_TextMeshProUGUI;
+    [SerializeField] private TextMeshProUGUI creditsTextComponent;
 
     void Start()
     {
-        string filePath = Path.Combine(Application.dataPath, "credits.txt");
-        if (File.Exists(filePath))
+        string creditsFilePath = Path.Combine(Application.dataPath, "credits.txt");
+        if (File.Exists(creditsFilePath))
         {
-            string text = File.ReadAllText(filePath);
-            m_TextMeshProUGUI.text = text;
-            Debug.Log(text);
+            string creditsText = File.ReadAllText(creditsFilePath);
+            creditsTextComponent.text = creditsText;
         }
         else
         {
-            Debug.LogError("File not found: " + filePath);
+            Debug.LogError("Credits file not found: " + creditsFilePath);
         }
     }
 }
