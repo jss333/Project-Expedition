3e2db764c1e907938da1a4e97390265e427bda04
jss333 <jss333@gmail.com>
2024-10-21 15:12:10 -0400

    Simplify how SFX are created and played via AudioManager

--- Assets/Scripts/AudioManagment/AudioManagerNoMixers.cs
+++ Assets/Scripts/AudioManagment/AudioManagerNoMixers.cs
@@ -24,24 +24,11 @@ public class AudioManagerNoMixers : MonoBehaviour {
     #endregion
 
     [Header("Pool")]
-    [SerializeField] private GameObject soundEffectPrefab;
+    [SerializeField] private GameObject randomPitchAudioSourcePrefab;
 
     [Header("SFX")]
-    private Dictionary<string, SFXAudioDataSO> sfxAudioClipMap = new Dictionary<string, SFXAudioDataSO>();
-
-    [Space(10)]
-    [Header("Music")]
-    [SerializeField] private bool muteMusic;
-    [SerializeField] private AudioSource musicSource;
-    [SerializeField] private List<AudioClip> musicAudioClips;
-
-    private readonly float maxVolume = 0.5f;
-    private bool isMusicOn = true;
-    private float currentMusicLength;
-    private int currentMusicIndex = 0;
-    private float startFadeOutAt_Percentage = 0.8f;
-    private float decreaseVolumeBy;
-    private float currentMusicTime;
+    private Dictionary<string, SFXAudioDataSO> sfxSOByName = new Dictionary<string, SFXAudioDataSO>();
+    private Dictionary<string, RandomPitchAudioSource> audioSrcByName = new Dictionary<string, RandomPitchAudioSource>();
 
     private void Start()
     {
@@ -53,82 +40,39 @@ public class AudioManagerNoMixers : MonoBehaviour {
         SFXAudioDataSO[] sfxAudioDataSOs = Resources.LoadAll<SFXAudioDataSO>("SFXSOs");
         foreach (SFXAudioDataSO audioDataSO in sfxAudioDataSOs)
         {
-            sfxAudioClipMap.Add(audioDataSO.SFXClipName, audioDataSO);
+            sfxSOByName.Add(audioDataSO.name, audioDataSO);
         }
-        Debug.Log("SFXAudioDataSOs loaded: " + sfxAudioClipMap.Count);
+        Debug.Log("SFXAudioDataSOs loaded: " + sfxSOByName.Count);
     }
 
-    public void PlaySFXByName(string sfxAudioClipName)
+    public void PlaySFXByName(string sfxName)
     {
-        SFXAudioDataSO clipToPlay = GetSFXSOFromName(sfxAudioClipName);
-        PlaySFX(clipToPlay);
-    }
-
-    private void PlaySFX(SFXAudioDataSO sFXAudioDataSO)
-    {
-        AudioSource sfxAudioSource = null;
-
-        for (int i = 0; i < transform.childCount; i++)
+        if(!sfxSOByName.ContainsKey(sfxName))
         {
-            if (transform.GetChild(i).name == sFXAudioDataSO.name)
-            {
-                sfxAudioSource = transform.GetChild(i).GetComponent<AudioSource>();
-                break;
-            }
-            
-            if(i == transform.childCount - 1)
-            {
-                GameObject newAudioSourceInstance = Instantiate(soundEffectPrefab, transform);
-                newAudioSourceInstance.name = sFXAudioDataSO.SFXClipName;
-                sfxAudioSource = newAudioSourceInstance.GetComponent<AudioSource>();
-
-                break;
-            }
+            Debug.LogError("Cannot play audio clip - SFXAudioDataSO with name " + sfxName + "not found");
+            return;
         }
-        
-        if(sfxAudioSource != null )
+
+        RandomPitchAudioSource audioSrc = GetOrCreateAudioSource(sfxSOByName[sfxName]);
+        audioSrc.PlayAssociatedAudio();
+    }
+
+    private RandomPitchAudioSource GetOrCreateAudioSource(SFXAudioDataSO sfxSO)
+    {
+        if (audioSrcByName.ContainsKey(sfxSO.name))
         {
-            sfxAudioSource.pitch = sFXAudioDataSO.GetRandomPitch();
-            sfxAudioSource.volume = sFXAudioDataSO.soundEffectVolume;
-            sfxAudioSource.PlayOneShot(sFXAudioDataSO.audioClip);
+            return audioSrcByName[sfxSO.name];
         }
-    }
+        else
+        {
+            GameObject newAudioSrc = Instantiate(randomPitchAudioSourcePrefab, transform);
+            newAudioSrc.name = sfxSO.name;
 
-    public void RunNextMusicClip()
-    {
-        currentMusicIndex++;
-        if (currentMusicIndex >= musicAudioClips.Count)
-            currentMusicIndex = 0;
+            RandomPitchAudioSource rndPitchAudioSrc = newAudioSrc.GetComponent<RandomPitchAudioSource>();
+            rndPitchAudioSrc.SetAssociatedAudio(sfxSO);
 
-        musicSource.clip = musicAudioClips[currentMusicIndex];
-        currentMusicLength = musicAudioClips[currentMusicIndex].length;
-        decreaseVolumeBy = maxVolume / ((1 - startFadeOutAt_Percentage) * musicAudioClips[currentMusicIndex].length);
-        musicSource.volume = 0;
-        musicSource.Play();
-    }
-
-    [ContextMenu("CalmMusic")]
-    public void PlayCalmMusic()
-    {
-        musicSource.clip = musicAudioClips[0];
-        currentMusicLength = musicAudioClips[0].length;
-        //decreaseVolumeBy = maxVolume / ((1 - startFadeOutAt_Percentage) * musicAudioClips[0].length);
-        musicSource.volume = PlayerPrefs.GetFloat("SFXVol");
-        musicSource.Play();
-    }
-
-    [ContextMenu("ActionMusic")]
-    public void PlayActionMusic()
-    {
-        musicSource.clip = musicAudioClips[1];
-        currentMusicLength = musicAudioClips[1].length;
-        //decreaseVolumeBy = maxVolume / ((1 - startFadeOutAt_Percentage) * musicAudioClips[1].length);
-        musicSource.volume = PlayerPrefs.GetFloat("MusicVol");
-        musicSource.Play();
-    }
-
-    private SFXAudioDataSO GetSFXSOFromName(string sfxName)
-    {
-        return sfxAudioClipMap[sfxName];
+            audioSrcByName.Add(sfxSO.name, rndPitchAudioSrc);
+            return rndPitchAudioSrc;
+        }
     }
 }
--- Assets/Scripts/AudioManagment/SFXAudioDataSO.cs
+++ Assets/Scripts/AudioManagment/SFXAudioDataSO.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New SFX", menuName = "Audio System/New SFX")]
 public class SFXAudioDataSO : ScriptableObject
 {
-    public string SFXClipName;
     public AudioClip audioClip;
     public float minPitch = 0.95f;
     public float maxPitch = 1.05f;
     public float soundEffectVolume = 1f;
-    private System.Random random;
-
-    public float GetRandomPitch()
-    {
-        random = new System.Random();
-        return (float)(minPitch + (random.NextDouble() * (maxPitch - minPitch)));
-    }
 }
