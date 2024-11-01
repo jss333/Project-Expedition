079c8d50466eb4bd6da5880638123c93594ff159
MostafaEbeed <en.mostfa4@gmail.com>
2024-11-01 14:28:32 +0200

    CreatingTestScene

---
+++ Assets/Scripts/Hazards/Platforms/HazardPlatform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HazardPlatform : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
